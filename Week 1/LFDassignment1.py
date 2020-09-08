98005863434e6e352570a01421ec40c85673007a
RobinNicolai97 <48721583+RobinNicolai97@users.noreply.github.com>
2020-09-08 11:20:42 +0200

    Update LFDassignment1.py
    assignment 1.1 complete (please check)

--- Week 1/LFDassignment1.py
+++ Week 1/LFDassignment1.py
@@ -5,6 +5,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 
 # COMMENT THIS
+# This function takes the corpus textfile as an input, together with a Boolean expression (True/False)
+# If use_sentiment == True, the script will classify reviews as positive or negative. If use_sentiment == False, it will classify reviews into six possible categories. 
+# It takes the label (the to be predicted class) out of each line in the tokenized textfile ( tokens[1] for sentiment, tokens[0] for category ) 
+# together with the textual content of the review: token[3:].
+# The function returns a list of textual reviews and a list of their corresponding labels.
+
+
 def read_corpus(corpus_file, use_sentiment):
     documents = []
     labels = []
@@ -28,6 +35,9 @@ def identity(x):
     return x
 
 # COMMENT THIS
+# The function read_corpus is activated, so that X == the textual content of the reviews and Y == their corresponding labels.
+# The data is divided into 75% training-data and 25% test-data. 
+
 X, Y = read_corpus('trainset.txt', use_sentiment=True)
 split_point = int(0.75*len(X))
 Xtrain = X[:split_point]
@@ -53,11 +63,15 @@ classifier = Pipeline( [('vec', vec),
 
 
 # COMMENT THIS
+# The Naive Bayes classifier takes the textual content of the reviews and their corresponding classes. 
+# It will train on right combinations of review and classes. Then it will be able to make predictions about the classes of not-seen-before review texts. 
 classifier.fit(Xtrain, Ytrain)
 
 # COMMENT THIS  
+# The Naive Bayes classifier predicts the classes of all reviews in the test-data. 
 Yguess = classifier.predict(Xtest)
 
 # COMMENT THIS
+# a simple accuracy measure is taken. This score is basicly how many times Ytest[X] == Yguess[X], divided by the length of Ytest and Yguess (which are both the same length).
 print(accuracy_score(Ytest, Yguess))
 
